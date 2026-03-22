699c9bf17f31821e60505df5470df19decade7d0
TH3 M0ST L3G3ND M!!CKY D0N 1NS!!D3 <akashakash336788@gmail.com>
2026-03-22 20:04:51 +0530

    Create package.json for Henry-X bot
    Initial package.json file for the Henry-X bot project, including dependencies and scripts.

---
+++ package.js
@@ -0,0 +1,27 @@
+{
+  "name": "henry-x-bot",
+  "version": "1.0.0",
+  "description": "Facebook Messenger Bot with Web Panel (HENRY-X)",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "keywords": [
+    "facebook-bot",
+    "messenger-bot",
+    "express",
+    "ws3-fca",
+    "henry-x"
+  ],
+  "author": "Henry-X",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.19.2",
+    "body-parser": "^1.20.2",
+    "ws3-fca": "latest"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.3"
+  }
+}
